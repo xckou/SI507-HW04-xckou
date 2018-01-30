9c99380e7c07267e22249c11621a305f06dbf5c6
G-bgyl <gelinyan0801@foxmail.com>
2018-01-30 17:17:58 -0500

    alicia quetion add add again

--- magic_eight.py
+++ magic_eight.py
@@ -2,7 +2,7 @@ import random
 
 def question():
     answer=input("What is your question?")
-    strrr="● Cannot predict now● Concentrate and ask again● Don't count on it● My reply is no● My sources say no● Outlook not so good● Very doubtful"
+    strrr="● It is certain● It is decidedly so● Without a doubt● Cannot predict now● Concentrate and ask again● Don't count on it● My reply is no● My sources say no● Outlook not so good● Very doubtful"
     ans_list=strrr.split('●')
     final_answer=random.choice(ans_list)
     return final_answer
