4947fc968976610cac8ab91f56c72944fd17eb86
Kangning <admin@admindeMacBook-Air-3.local>
2018-01-30 17:15:03 -0500

    Add more questions

--- magic_eight.py
+++ magic_eight.py
@@ -1,5 +1,6 @@
 import random
 
+
 def question():
 	answer = input("What is your question?")
 	ans_list = ["Yes definitely", "You may rely on it", "As I see it, yes", "Most likely", "Outlook good", "Yes", "Signs point to yes", "Reply hazy try again", "Ask again later", "Better not tell you now"]
