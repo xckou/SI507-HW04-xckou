93f722eb61e54906321501b8895636b93d9b8998
G-bgyl <gelinyan0801@foxmail.com>
2018-01-30 17:15:29 -0500

    Alicia add question again

--- magic_eight.py
+++ magic_eight.py
@@ -1,2 +1,10 @@
+import random
+
 def question():
-	answer = input("What is your question?")
+    answer=input("What is your question?")
+    strrr="● Cannot predict now● Concentrate and ask again● Don't count on it● My reply is no● My sources say no● Outlook not so good● Very doubtful"
+    ans_list=strrr.split('●')
+    final_answer=random.choice(ans_list)
+    return final_answer
+
+question()
