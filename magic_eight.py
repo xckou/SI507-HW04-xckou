9392158b2c24fcd535bf33416a2e4e0bcc22587f
xckou <xqquan616@gmail.com>
2018-01-30 17:07:17 -0500

    checkquestion1

--- magic_eight.py
+++ magic_eight.py
@@ -1,2 +1,11 @@
 def question():
 	answer = input("What is your question?")
+	if answer[-1] != "?":
+		print("I’m sorry, I can only answer questions.")
+	while answer !="quit":
+		answer = input("What is your question?")
+		if answer == "quit":
+			return
+		elif answer[-1] != "?":
+			print("I’m sorry, I can only answer questions.")
+question()
